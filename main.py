f93c4a0ddb3c978e0a7503008a55c6317c9309e8
Keslon Silva <kesloni40@gmail.com>
2023-06-06 00:15:27 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#importa a biblioteca Random
+import random
+#pede para o usuario digitar os nomes
+names_string = input("Qual a lista de pessoas que podem ser escolhidas ? ")
+#junta os nomes em uma lista
+names = names_string.split(", ")
+#mede quantos itens tem na lista
+num_itens = len(names)
+#gera um numero aleatorio dentro da quantidade de itens da lista
+random_choice = random.randint(0, num_itens -1)
+#printa o nome correspondente a posição gerada aleatoriamente
+print(names[random_choice])
